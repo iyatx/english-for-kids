d8ef8ca1e74990628be8664331e5cf8f5aede5cb
AzizbekSavkimov <azizbeksavkimov@mail.ru>
2021-07-10 09:55:14 +0500

    fix: difficult page with sound

--- src/pages/difficult/difficult.tsx
+++ src/pages/difficult/difficult.tsx
@@ -6,10 +6,8 @@ import { ICard } from '@interfaces/index';
 
 import './difficult.scss';
 
-export const DifficultPage = () => {
-  const mode = useSelector((state: AppState) => state.app.mode);
+export const DifficultPage: React.FC = () => {
   const words = useSelector((state: AppState) => state.statistics.items);
-
   const [items, setItems] = useState<ICard[]>([]);
 
   useEffect(() => {
@@ -24,18 +22,20 @@ export const DifficultPage = () => {
 
   return (
     <div className="difficult container">
-      <h2 className="text-lg">Difficult Words</h2>
-      {items.length === 0 ? (
-        <div className="difficult__not-words">
-          <img src="assets/images/empty-page.jpg" alt="" />
-        </div>
-      ) : (
-        <div className="difficult__container">
-          {items.map((item, index) => (
-            <GameCard card={item} mode={mode} key={index} />
-          ))}
-        </div>
-      )}
+      <div className="container">
+        <h2 className="text-lg">Difficult Words</h2>
+        {items.length === 0 ? (
+          <div className="difficult__not-words">
+            <img src="assets/images/empty-page.jpg" alt="" />
+          </div>
+        ) : (
+          <div className="difficult__container">
+            {items.map((item, index) => (
+              <GameCard card={item} mode="TRAIN" key={index} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
